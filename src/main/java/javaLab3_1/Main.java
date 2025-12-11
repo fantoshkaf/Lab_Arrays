e0b361a27cdd6985c8dd3cbc5855fd37ad2e9db5
fantoshkaf <fantoshkaf@mail.ru>
2025-12-11 21:36:44 +0300

    doc

--- src/main/java/javaLab3_1/Main.java
+++ src/main/java/javaLab3_1/Main.java
@@ -1,10 +1,11 @@
 package javaLab3_1;
 
 import java.util.Random;
-import java.util.Scanner;
 
 /**
- * Программа для выполнения простых операций калькулятора и поиска максимального элемента массива.
+ * Программа генерирует массив из 20 целых числе в диапазоне от -10 до 10
+ * Затем находит максимальный отрицательный и минимальный положительный элементы массива
+ * После чего меняет их местами
  *
  * @author Anton Fedyaev
  * @version 2.0
@@ -14,38 +15,44 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = new int[20];
         Random random = new Random();
-        int maxNegative = -11;
-        int minPositive = 11;
-        int maxNegativeNumber=0;
-        int minPositiveNumber=0;
+        //-11 и 11 не могут попасть в массив
+        int maxNegative = -11;// максимальный отрицательный
+        int minPositive = 11;// минимальный положительный
+        int maxNegativeNumber = -1;// номер максимального отрицательного
+        int minPositiveNumber = -1;// номер минимального положительного
         System.out.println("Исходный массив:");
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(21) - 10;
+            arr[i] = random.nextInt(21) - 10;// от -10 до 10 включительно
             System.out.println("arr[" + i + "]" + arr[i]);
+
+            // поиск минимального положительного
             if (arr[i] > 0 && arr[i] < minPositive) {
                 minPositive = arr[i];
-                maxNegativeNumber=i;
+                maxNegativeNumber = i;
                 continue;
             }
 
+            // поиск максималного отрицательного
             if (arr[i] < 0 && arr[i] > maxNegative) {
                 maxNegative = arr[i];
-                minPositiveNumber=i;
+                minPositiveNumber = i;
 
             }
 
         }
-        System.out.println();
 
+        System.out.println();// пустая строка для красоты
+
+
+        // проверка, что нужные элементы найдены
         if (maxNegative == -11 || minPositive == 11) {
             System.out.println("Положительные или отрицательные элементы не найдены");
 
-        }
-
-        else {
-            arr[maxNegativeNumber]=maxNegative;
-            arr[minPositiveNumber]=minPositive;
-            System.out.println("Новый массив, местами поменялись элементы "+maxNegativeNumber+" и "+minPositiveNumber+":");
+        } else {
+            // элементы меняются местами
+            arr[maxNegativeNumber] = maxNegative;
+            arr[minPositiveNumber] = minPositive;
+            System.out.println("Новый массив, местами поменялись элементы " + maxNegativeNumber + " и " + minPositiveNumber + ":");
             for (int i = 0; i < arr.length; i++) {
                 System.out.println("arr[" + i + "]" + arr[i]);
 
